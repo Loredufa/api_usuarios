b680a759a8a6ffbe611c7c390ef25eeb142f2013
LoreDufa <lorenadufaur@gmail.com>
2023-10-31 21:58:04 -0300

    prueba

--- src/routes/getUsuario.js
+++ src/routes/getUsuario.js
@@ -1,13 +1,16 @@
 const { Router } = require('express');
 const {getAllUsuario, addUsuario, getUsuarioById, putUsuario, deleteUsuario, getUsuarioByLogin, putUsuarioMod} = require('../controllers/Usuarios')
-//const verifyRoleAdministrador = require('../controllers/role/VerifyRoleAdministrador');
+const {getUserByUserapp} = require('../controllers/RecupPassApp')
+const {getUserByUsername} = require('../controllers/RecuperoPass')
 const verifyToken = require('../utils/middlewares/verifyToken');
 
 const router = Router();
 
-router.get('/', getAllUsuario)
+router.get('/', verifyToken, getAllUsuario)
 router.get('/:id',verifyToken, getUsuarioById)
-router.post('/', verifyToken, addUsuario)
+router.post('/', verifyToken, addUsuario);
+router.post('/resetapp', verifyToken, getUserByUserapp);
+router.post('/reset', verifyToken, getUserByUsername);
 router.put('/', verifyToken, putUsuario);
 router.put('/:id', verifyToken, putUsuarioMod);
 router.delete('/:id', verifyToken, deleteUsuario);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const verifyToken = require('../utils/middlewares/verifyToken');
+//const verifyToken = require('../utils/middlewares/verifyToken');
 
 const viajeRoute = require('./getUsuario');
 const resetRoute = require('./resetPassword');
 const resetRouteApp = require('./resetApp');
 
 
-router.use('/usuarios', verifyToken, viajeRoute)
-router.use('/reset', verifyToken, resetRoute)
-router.use('/resetapp', verifyToken, resetRouteApp)
+router.use('/usuarios', viajeRoute)
+//router.use('/reset', verifyToken, resetRoute)
+//router.use('/resetapp', verifyToken, resetRouteApp)
 
 
 module.exports = router;
