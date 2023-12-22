f490336d181e23d499c77939faa6ff90ba827469
LoreDufa <lorenadufaur@gmail.com>
2023-12-22 07:31:43 -0300

    error itinerario

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,22 +1,19 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = function(sequelize) {
-  // defino el modelo
   return sequelize.define('schedule', {
-    id:  {
+    id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true
     },
     nombre: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT, 
       allowNull: false,
     },
     texto_gral: {
-      type:DataTypes.STRING,
-      allowNull:true,
+      type: DataTypes.JSON,
+      allowNull: true,
     }
-  })
-
-  };
+  });
+};
